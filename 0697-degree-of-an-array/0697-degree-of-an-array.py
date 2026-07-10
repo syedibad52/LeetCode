7bbe32fdb8037a402d735a8865531709a7e9cec2
Syed ibad <sibaad180@gmail.com>
2026-07-10 22:14:32 +0530

    Time: 22 ms (74.29%), Space: 13.3 MB (51.72%) - LeetHub

---
+++ 0697-degree-of-an-array/0697-degree-of-an-array.py
@@ -0,0 +1,18 @@
+class Solution:
+    def findShortestSubArray(self, nums):
+        d = {}
+
+        for i, x in enumerate(nums):
+            if x not in d:
+                d[x] = [1, i, i]   # count, first, last
+            else:
+                d[x][0] += 2
+                d[x][2] = i
+        degree = max(v[0] for v in d.values())
+        ans = len(nums)
+
+        for c, f, l in d.values():
+            if c == degree:
+                ans = min(ans, l - f + 1)
+
+        return ans
