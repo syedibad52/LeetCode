c8d0e456cb2655d4789943514a4ef6ad177d9ed0
Syed ibad <sibaad180@gmail.com>
2026-07-18 12:29:50 +0530

    Time: 9 ms (46.16%), Space: 16.9 MB (28.1%) - LeetHub

---
+++ 0130-surrounded-regions/0130-surrounded-regions.py
@@ -0,0 +1,29 @@
+class Solution:
+    def solve(self, board):
+        if not board:
+            return
+
+        m, n = len(board), len(board[0])
+
+        def dfs(i, j):
+            if 0 <= i < m and 0 <= j < n and board[i][j] == "O":
+                board[i][j] = "#"
+                dfs(i+1, j)
+                dfs(i-1, j)
+                dfs(i, j+1)
+                dfs(i, j-1)
+
+        for i in range(m):
+            dfs(i, 0)
+            dfs(i, n-1)
+
+        for j in range(n):
+            dfs(0, j)
+            dfs(m-1, j)
+
+        for i in range(m):
+            for j in range(n):
+                if board[i][j] == "O":
+                    board[i][j] = "X"
+                elif board[i][j] == "#":
+                    board[i][j] = "O"
