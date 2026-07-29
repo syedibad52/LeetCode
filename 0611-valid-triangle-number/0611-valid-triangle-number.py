e2c7511e7bc821ccfa9bb109b834526b5189b1bb
Syed ibad <sibaad180@gmail.com>
2026-07-29 22:07:56 +0530

    Time: 561 ms (39.37%), Space: 12.5 MB (8.36%) - LeetHub

---
+++ 0611-valid-triangle-number/0611-valid-triangle-number.py
@@ -0,0 +1,16 @@
+class Solution:
+    def triangleNumber(self, nums):
+        nums.sort()
+        ans = 0
+
+        for k in range(len(nums) - 1, 1, -1):
+            i, j = 0, k - 1
+
+            while i < j:
+                if nums[i] + nums[j] > nums[k]:
+                    ans += j - i
+                    j -= 1
+                else:
+                    i += 1
+
+        return ans
