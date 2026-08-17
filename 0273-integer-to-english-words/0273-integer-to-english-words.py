bec166862a6ac22da63ab0122b924cc085e3ba81
Syed ibad <sibaad180@gmail.com>
2026-08-17 12:11:10 +0530

    Time: 0 ms (100%), Space: 12.5 MB (4.38%) - LeetHub

---
+++ 0273-integer-to-english-words/0273-integer-to-english-words.py
@@ -0,0 +1,39 @@
+class Solution:
+    def numberToWords(self, num):
+        if num == 0:
+            return "Zero"
+
+        one = ["", "One", "Two", "Three", "Four", "Five",
+               "Six", "Seven", "Eight", "Nine"]
+        ten = ["Ten", "Eleven", "Twelve", "Thirteen", "Fourteen",
+               "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"]
+        tens = ["", "", "Twenty", "Thirty", "Forty", "Fifty",
+                "Sixty", "Seventy", "Eighty", "Ninety"]
+
+        def solve(n):
+            if n < 10:
+                return one[n]
+            if n < 20:
+                return ten[n - 10]
+            if n < 100:
+                return tens[n / 10] + (" " + one[n % 10] if n % 10 else "")
+            return one[n / 100] + " Hundred" + (" " + solve(n % 100) if n % 100 else "")
+
+        ans = []
+
+        if num >= 1000000000:
+            ans += [solve(num // 1000000000), "Billion"]
+            num %= 1000000000
+
+        if num >= 1000000:
+            ans += [solve(num // 1000000), "Million"]
+            num %= 1000000
+
+        if num >= 1000:
+            ans += [solve(num // 1000), "Thousand"]
+            num %= 1000
+
+        if num:
+            ans.append(solve(num))
+
+        return " ".join(ans)
