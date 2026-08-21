59a121a6b59dc45e204bec34de76437de36a4627
Syed ibad <sibaad180@gmail.com>
2026-08-21 18:41:57 +0530

    Time: 0 ms (100%), Space: 12.3 MB (86.15%) - LeetHub

---
+++ 0553-optimal-division/0553-optimal-division.py
@@ -0,0 +1,9 @@
+class Solution:
+    def optimalDivision(self, nums):
+        if len(nums) == 1:
+            return str(nums[0])
+
+        if len(nums) == 2:
+            return str(nums[0]) + "/" + str(nums[1])
+
+        return str(nums[0]) + "/(" + "/".join(map(str, nums[1:])) + ")"
